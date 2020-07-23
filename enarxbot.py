b7bd188afb130f51d004857205375f81ca9aa406
Nathaniel McCallum <npmccallum@redhat.com>
2020-07-23 08:48:14 -0400

    Add enarxbot.connect()
    This can contain all our connection logic.

--- enarxbot.py
+++ enarxbot.py
@@ -1,6 +1,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import github
+import os
+
+def connect():
+    token = os.environ.get('GITHUB_TOKEN', None)
+    return github.Github(token)
 
 def create_card(column, content_id, content_type):
     try:	

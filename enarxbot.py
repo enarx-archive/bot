8788b8eff7a568c4fea67223c3995512f916a7ee
Nathaniel McCallum <npmccallum@redhat.com>
2020-07-23 08:57:23 -0400

    Add a helper function for graphql queries

--- enarxbot.py
+++ enarxbot.py
@@ -1,11 +1,26 @@
 # SPDX-License-Identifier: Apache-2.0
 
+import requests
 import github
 import os
 
 def connect():
     token = os.environ.get('GITHUB_TOKEN', None)
     return github.Github(token)
+
+def graphql(query):
+    token = os.environ.get('GITHUB_TOKEN', None)
+    json = { "query": query }
+    headers = {}
+
+    if token is not None:
+        headers["Authorization"] = f"token {token}"
+
+    reply = requests.post("https://api.github.com/graphql", json=json, headers=headers)
+    if reply.status_code == 200:
+        return reply.json()
+
+    raise Exception(f"Query failed: {reply.status_code}! {query}")
 
 def create_card(column, content_id, content_type):
     try:
